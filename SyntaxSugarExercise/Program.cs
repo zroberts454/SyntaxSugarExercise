918dc56cd0e308dba699512e1010c93294bef330
zroberts454 <zachary123roberts@gmail.com>
2021-02-24 19:05:17 -0600

    intial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var answer = int.Parse(Console.ReadLine());
+            
+            var response = (answer > 9) ? $"{answer} is higher than 9!" : $"{answer} is lower than 9!";
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
